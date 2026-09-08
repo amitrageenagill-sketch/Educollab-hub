48d6b5d4b6efc4140ec6511f4f1ce609f35d17eb
amitrageenagill-sketch <amitrageenagill@gmail.com>
2026-09-08 19:50:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 from google.oauth2 import service_account
 
 # --- Firebase Firestore Setup using Streamlit Secrets ---
-key_dict = json.loads(st.secrets["textkey"]["json_key"])
+key_dict = dict(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project=creds.project_id)
 # --- Streamlit Page Configuration ---
